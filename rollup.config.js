f297efccaa78b04e3d2f14b845328a3652c03adb
Rogerio Carvalho <rogerio.w.carvalho@gmail.com>
2017-11-16 22:48:12 +1100

    Add bundler

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+export default {
+    input: 'src/app.js',
+    output: {
+        file: 'dist/app.js',
+        format: 'umd',
+        name: 'Jars',
+        sourcemap: 'inline',
+    },
+    plugins: [
+        resolve(),
+        babel({ exclude: 'node_modules/**' }),
+    ],
+    external: ['lodash'],
+};
